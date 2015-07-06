9c1b50749198bc5878cd5a5076ee0a79675882ec
Robin <robimeht@umich.edu>
2015-07-05 22:59:59 -0700

    Deleted flatUIKit, got rid of Tutorial json + image files in build phases' (copy bundles & resources), silenced some warnings.

--- Bounce/Bounce/ViewControllers/IntoductionView/FacebookLogin.m
+++ Bounce/Bounce/ViewControllers/IntoductionView/FacebookLogin.m
@@ -15,20 +15,6 @@
 #import "UIViewController+AMSlideMenu.h"
 
 #import <UIKit/UIKit.h>
-#import "FlatUIKit.h"
-#import "UIColor+FlatUI.h"
-#import "UISlider+FlatUI.h"
-#import "UIStepper+FlatUI.h"
-#import "UITabBar+FlatUI.h"
-#import "UINavigationBar+FlatUI.h"
-#import "FUIButton.h"
-#import "FUISwitch.h"
-#import "UIFont+FlatUI.h"
-#import "FUIAlertView.h"
-#import "UIBarButtonItem+FlatUI.h"
-#import "UIProgressView+FlatUI.h"
-#import "FUISegmentedControl.h"
-#import "UIPopoverController+FlatUI.h"
 
 #import "AFNetworking.h"
 #import "pushnotification.h"
--- Bounce/Bounce/ViewControllers/MessageScreenViewController.h
+++ Bounce/Bounce/ViewControllers/MessageScreenViewController.h
@@ -6,19 +6,6 @@
 //  Copyright (c) 2015 hobble. All rights reserved.
 //
 #import <UIKit/UIKit.h>
-#import "UIColor+FlatUI.h"
-#import "UISlider+FlatUI.h"
-#import "UIStepper+FlatUI.h"
-#import "UITabBar+FlatUI.h"
-#import "UINavigationBar+FlatUI.h"
-#import "FUIButton.h"
-#import "FUISwitch.h"
-#import "UIFont+FlatUI.h"
-#import "FUIAlertView.h"
-#import "UIBarButtonItem+FlatUI.h"
-#import "UIProgressView+FlatUI.h"
-#import "FUISegmentedControl.h"
-#import "UIPopoverController+FlatUI.h"
 #import "NIDropDown.h"
 #import "ParseManager.h"
 #import "RequestManger.h"
